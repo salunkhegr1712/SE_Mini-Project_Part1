553d0dfdc5de1b52f0782869933f5afb1f44c170
salunkhegr1712 <salunkhegr19.comp@coep.ac.in>
2021-08-09 20:04:10 +0530

    first time1

---
+++ new.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int a;
+	int b;
+	cout<<"enter first number::"<<endl;
+	cin>>a;
+	cout<<"enter second number::"<<endl;
+	cin >>b;
+	cout<<"adition is ::"<<a+b<<endl;
+	cout<<"substraction is::"<<a-b<<endl;
+	cout<<"multiplicatuon is::"<<a*b<<endl;
+	cout<<"floor division is::"<<a/b<<endl;
+	return 0;
+
+	}
